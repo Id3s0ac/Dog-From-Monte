568bc1d34fb501cd583cf81b14ccd99a2e8e8a51
Id3s0ac <37808149+Id3s0ac@users.noreply.github.com>
2018-03-31 17:28:33 +0200

    Update dfm.js

--- dfm.js
+++ dfm.js
@@ -1,3 +1,4 @@
+
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
@@ -7,32 +8,8 @@ client.on('ready', () => {
 
 client.on('message', message => {
     if (message.content === 'Heil!') {
-    	message.reply('HEIL HITLER!');
+    	message.reply('Hitler!');
   	}
-});
-
-client.on('message', message => {
-    if (message.content === 'Heil!') {
-    	message.reply('Dobra, już sklej pizde');
-  	}
-});
-
-client.on('message', message => {
-    if (message.content === 'info') {
-    	message.reply('Właśnie rucham ci starą');
-  	}
-});
-
-client.on('message', message => {
-    if (message.content === 'Spierdalaj') {
-    	message.reply('Luzuj suty XDDDDDD');
-  	}
-});
-
-client.on('message', message => {
-    if (message.content === 'Japierdole') {
-    	message.reply('https://i.pinimg.com/736x/47/45/98/47459881654dacb329ba729f04011c26.jpg');
-
 });
 
 // THIS  MUST  BE  THIS  WAY
