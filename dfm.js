afc4997e0f2639b2fd808de34cd88df6b3397688
Id3s0ac <37808149+Id3s0ac@users.noreply.github.com>
2018-03-31 17:24:50 +0200

    Update dfm.js

--- dfm.js
+++ dfm.js
@@ -17,5 +17,23 @@ client.on('message', message => {
   	}
 });
 
+client.on('message', message => {
+    if (message.content === 'info') {
+    	message.reply('Właśnie rucham ci starą');
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === 'Spierdalaj') {
+    	message.reply('Luzuj suty XDDDDDD');
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === 'Japierdole') {
+    	message.reply('https://i.pinimg.com/736x/47/45/98/47459881654dacb329ba729f04011c26.jpg');
+
+});
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
