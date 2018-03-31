dcdf7cf2c4a884463553fe6a609b1095b3be0dbf
Id3s0ac <37808149+Id3s0ac@users.noreply.github.com>
2018-03-31 18:22:42 +0200

    Update dfm.js

--- dfm.js
+++ dfm.js
@@ -12,16 +12,5 @@ client.on('message', message => {
   	}
 });
 
- if (command === 'info') {
-        let embed = new Discord.RichEmbed();
-        embed.setThumbnail(client.user.avatarURL);
-        embed.addField('Users', client.users.size, true);
-        embed.addField('Servers', client.guilds.size, true);
-        embed.addField('Creator', '<@218397146049806337>', true);
-        embed.addField("Invite", 'http://bit.ly/inviteKhanbot', true);
-        embed.setColor('#00ffcc');
-        message.channel.send({ embed })
- });
-
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
